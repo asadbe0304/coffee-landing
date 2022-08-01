52500760f7deb9a8b4255a71a028271389958af0
asadbe0304 <asadbekaxmadkulov@gmail.com>
2022-08-01 11:17:26 +0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var elMenu = document.querySelector(".hamburger")
+var elModal = document.querySelector(".modal")
+elmenu.addEventListener("click", function(){
+    elmodal.classList.toggle("active")
+})
+console.log("Menu")
